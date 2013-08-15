3acfb76ac1628d307c7183d514f8e5a13b633e28
mattlykins <matt@mattlykins.com>
2013-08-15 09:44:46 -0400

    Clean up

--- Adf04ToStout/adf042stout.py
+++ Adf04ToStout/adf042stout.py
@@ -2,14 +2,17 @@
 '''
 Created on August 14, 2013
 
-@author: Matt
+@author: Matt Lykins
 '''
 import sys
 import datetime
 import fractions
 import re
 
+# Scientific notation values are missing E. True means that we should add an E
 missingE = True
+
+DEBUGMODE = False
 
 # Test whether a string can be a number
 def is_number(x):
@@ -27,18 +30,14 @@ def add_e(string):
         newstring = string
     return newstring
 
+# This is the screen reached by running the program with -? or /?
 def run_help():
     print("This program converts atomic data from the ADF04 format to Stout format.\n")
-    print("Syntax: adf042stout.py <Filename> <StatWeight True:False>  <MissingE True:False>\n")
+    print("Syntax: adf042stout.py <Filename> \n")
     print("Filename = The ADF04 file you want to process. The default is test.dat\n")
-    print("StatWeight = ADF04 is supposed to list the statistical weight. Sometimes they list J instead. The default is False (assume they gave J)\n")
-    print("MissingE = Some files are missing the E on number in scientific notation. The default is True (E is missing)")
-
-DEBUGMODE = False
-ADF04StatWt = False
 
 
-
+# Control the command line parameters sent to the program
 if len(sys.argv) < 2:
     print("Filename not specified")
     print("Looking for test.dat")
@@ -49,31 +48,11 @@ elif len(sys.argv) == 2:
         run_help()
         sys.exit(0)
     adf04_file_name = str(sys.argv[1])
-    print("Processing %s" % adf04_file_name)
-elif len(sys.argv) == 3:
-    adf04_file_name = str(sys.argv[1])
-    if str(sys.argv[2]).upper() == "TRUE":
-        ADF04StatWt = True
-    else:
-        ADF04StatWt = False
-        
-    print("Processing %s with StatWt = %s" % (adf04_file_name,ADF04StatWt))
-else:
-    adf04_file_name = str(sys.argv[1])
-    if str(sys.argv[2]).upper() == "TRUE":
-        ADF04StatWt = True
-    else:
-        ADF04StatWt = False
-        
-    if str(sys.argv[3]).upper() == "TRUE":
-        missingE = True
-    else:
-        missingE = False
-        
-    print("Processing %s with StatWt = %s and missingE = %s" % (adf04_file_name,ADF04StatWt,missingE))
+    print("Processing %s" % adf04_file_name)  
 
 
 
+# Determine output file names
 base_name = adf04_file_name.split(".")[0]
 
 energy_output_name = base_name + ".nrg.txt"
@@ -86,6 +65,7 @@ colpos_config = 24 #5+19
 colpos_term = 34  #5+19+10
 colpos_energy = 50  #5+19+10+16
 
+# Energy Level Arrays
 index = []
 energy = []
 configuration = []
@@ -93,6 +73,7 @@ term = []
 termToken = []
 statwt = []
 
+# Radiative and Collision Data Arrays
 temps = []
 levhi = []
 levlo = []
@@ -106,11 +87,13 @@ except IOError:
     print("PROBLEM: cannot open %s" % adf04_file_name)
     sys.exit(10)
     
-
+# We need to be able to skip the first line of the file
 firstline = True
+# Are we reading energy data for RadCol Data?
 readEnergyData = True
+# Is this a temperature line?
 temperatureLine = True
-countTrans = 0
+
 for current_line in adf04_file:
     if firstline:
         firstline = False
@@ -126,8 +109,8 @@ for current_line in adf04_file:
         tempIndex = tempString.strip()
             
         #print("Index = %s" % tempIndex)
-        
-        
+                
+        # -1 denotes the end of the energy level section
         if tempIndex == "-1":
             readEnergyData = False
             continue        
@@ -151,14 +134,10 @@ for current_line in adf04_file:
         #print("Term = %s" % tempTerm)
         
         #Get stat weight
-        #PROBLEM: ADF04 supposed to have multiplicity 2s+1. Several test files list J instead
         tempTermMod = tempTerm.replace('('," ").replace(')'," ")
         termToken = tempTermMod.split()
         
-        if ADF04StatWt:
-            tempStatWt = float(termToken[len(termToken)-1])
-        else:
-            tempStatWt = 2*float(termToken[len(termToken)-1])+1
+        tempStatWt = 2*float(termToken[len(termToken)-1])+1
         
         #print("StatWt = %s" % tempStatWt)
         
@@ -172,10 +151,11 @@ for current_line in adf04_file:
         #print("Energy = %s" % tempEnergy)
        
         #************************************************
-        #if DEBUGMODE:
-        #    print("FERRET:\t%s\t%s\t%s\t%s" % (tempIndex,tempConfig,tempTerm,tempEnergy))
+        if DEBUGMODE:
+            print("DEBUG:\t%s\t%s\t%s\t%s" % (tempIndex,tempConfig,tempTerm,tempEnergy))
         
         
+        # Add values to the energy level arrays
         index.append(int(tempIndex))
         configuration.append(tempConfig)
         term.append(tempTerm)
@@ -185,20 +165,25 @@ for current_line in adf04_file:
     #**************************************************
     # Read the rest of the file. Radiative and Collision data    
     else:
+        # There are no extra spaces so we can just use split instead of column positions
         line_list = current_line.split()
         
+        # Again -1 denotes the end of the section
         if line_list[0].strip() == "-1":
             break
         
         if temperatureLine:
+            #There are 2 values on the line other than temps
             numTemps = len(line_list)-2
             for i in range(2,len(line_list)):
                 tempTemp = line_list[i]
+                # Fill in the missing E for scientific notation
                 tempTemp = add_e(tempTemp)
                 temps.append(float(tempTemp))
                 
             temperatureLine = False
         else:
+            
             tempLevHi = line_list[0]
             tempLevLo = line_list[1]
             tempEina = add_e(line_list[2])
@@ -213,7 +198,7 @@ for current_line in adf04_file:
                 sys.exit(1)
                 
                 
-            #Reset colls array
+            #Reset colls array for each row in the file
             colls = []
                 
             for i in range(3,len(line_list)-1):
@@ -226,15 +211,7 @@ for current_line in adf04_file:
             levlo.append(int(tempLevLo))
             eina.append(float(tempEina))
             cs.append(colls)
-        
-        
-    
-    
-    
-    
-    
-    
-
+            
 adf04_file.close()
 
 print ("Outputting to %s, %s, and %s\n" % (energy_output_name,tp_output_name,coll_output_name))
@@ -242,12 +219,12 @@ print ("Outputting to %s, %s, and %s\n" % (energy_output_name,tp_output_name,col
 #**************Write out energy file*********************
 energy_output = open(energy_output_name,"w")
 
-#Write out the magic number at the top of the file
+# Write out the magic number at the top of the file
 energy_output.write("11 10 14\n")
 
+# Write out the index, energy, statistical weight, configuration, and term
 for ndex,nrg,stwt,cfg,trm in zip(index,energy,statwt,configuration,term):
     energy_output.write("%i\t%.1f\t%i\t%s\t%s\n" % (ndex,nrg,stwt,cfg,trm))
-
 
 # Write out End of Data delimiter and Reference including the current date
 energy_output.write("**************\n#Reference:\n#")
@@ -260,12 +237,12 @@ energy_output.close()
 #**************Write out tp file*********************
 tp_output = open(tp_output_name,"w")
 
-#Write out the magic number at the top of the file
+# Write out the magic number at the top of the file
 tp_output.write("11 10 14\n")
 
+# Write out A, lo index, hi index, and Einstein A
 for ndexlo,ndexhi,eina in zip(levlo,levhi,eina):
     tp_output.write("A\t%i\t%i\t%.2e\n" % (ndexlo,ndexhi,eina))
-
 
 # Write out End of Data delimiter and Reference including the current date
 tp_output.write("**************\n#Reference:\n#")
@@ -278,35 +255,27 @@ tp_output.close()
 #**************Write out coll file*********************
 coll_output = open(coll_output_name,"w")
 
-#Write out the magic number at the top of the file
+# Write out the magic number at the top of the file
 coll_output.write("11 10 14\n")
 
-#Write out the temperature line
+# Write out the temperature line
 coll_output.write("TEMP")
 for i in range(0,len(temps)):
     coll_output.write("\t%.2e" % temps[i])
 coll_output.write("\n")
 
+# Write out the collision data
 for i in range(0,len(cs)):
     coll_output.write("CS ELECTRON\t%i\t%i" % (levlo[i],levhi[i]))
     for j in range(0, len(cs[i])):
         coll_output.write("\t%.2e" % cs[i][j])
     coll_output.write("\n")
     
-
-#for ndexlo,ndexhi,eina in zip(levlo,levhi,eina):
-#    coll_output.write("A\t%i\t%i\t%.2e\n" % (ndexlo,ndexhi,eina))
-
-
 # Write out End of Data delimiter and Reference including the current date
 coll_output.write("**************\n#Reference:\n#")
 date_today = datetime.date.today()
 coll_output.write(date_today.strftime("%Y-%m-%d"))
 
 coll_output.close()
-
-
-
-
 
 sys.exit(0)
